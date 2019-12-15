9517f1bd76bbf43d71b3eea1b5d0556abc82a2ec
James Morgan <james.morgan.e@googlemail.com>
2019-12-15 08:02:59 +0000

    Adding event handlers for all Auction events

--- src/mappings/auction-v2-mapping.ts
+++ src/mappings/auction-v2-mapping.ts
@@ -9,6 +9,7 @@ import {
     BidRejected,
     BidWithdrawn,
     BidIncreased,
+    AuctionCancelled,
 } from "../../generated/ArtistAcceptingBidsV2/ArtistAcceptingBidsV2";
 import {KnownOrigin} from "../../generated/KnownOrigin/KnownOrigin"
 
@@ -43,30 +44,85 @@ export function handleAuctionEnabled(event: AuctionEnabled): void {
 //      - Artist (or KO in very very small circumstances) accepts the current highest offer
 //      - monies split between artist/KO & optional commission split
 //      - no open offer remaining (new offers can now be made if not sold out)
+//
+// Cancelled = AuctionCancelled
+//      - BR triggerred function
+//      - refunds highest bidder
+
+// Ideas on what to store:
+// * auction history - single property pertaining to a running offer history against an edition
+// * total accepted ETH against an Edition = capture ETH value for when bids are accepted
+// * total rejected ETH against an Edition
+// * total numbers of offers made
 
 export function handleBidPlaced(event: BidPlaced): void {
+    /*
+      event BidPlaced(
+        address indexed _bidder,
+        uint256 indexed _editionNumber,
+        uint256 _amount
+      );
+    */
     let contract = KnownOrigin.bind(Address.fromString("0xFBeef911Dc5821886e1dda71586d90eD28174B7d"))
     let editionEntity = loadOrCreateEdition(event.params._editionNumber, contract)
 }
 
 export function handleBidAccepted(event: BidAccepted): void {
+    /*
+      event BidAccepted(
+        address indexed _bidder,
+        uint256 indexed _editionNumber,
+        uint256 indexed _tokenId,
+        uint256 _amount
+      );
+    */
     let contract = KnownOrigin.bind(Address.fromString("0xFBeef911Dc5821886e1dda71586d90eD28174B7d"))
     let editionEntity = loadOrCreateEdition(event.params._editionNumber, contract)
 }
 
 export function handleBidRejected(event: BidRejected): void {
+    /*
+      event BidRejected(
+        address indexed _caller,
+        address indexed _bidder,
+        uint256 indexed _editionNumber,
+        uint256 _amount
+      );
+    */
     let contract = KnownOrigin.bind(Address.fromString("0xFBeef911Dc5821886e1dda71586d90eD28174B7d"))
     let editionEntity = loadOrCreateEdition(event.params._editionNumber, contract)
 }
 
 export function handleBidWithdrawn(event: BidWithdrawn): void {
+    /*
+      event BidderRefunded(
+        uint256 indexed _editionNumber,
+        address indexed _bidder,
+        uint256 _amount
+      );
+    */
     let contract = KnownOrigin.bind(Address.fromString("0xFBeef911Dc5821886e1dda71586d90eD28174B7d"))
     let editionEntity = loadOrCreateEdition(event.params._editionNumber, contract)
 }
 
 export function handleBidIncreased(event: BidIncreased): void {
+    /*
+      event BidIncreased(
+        address indexed _bidder,
+        uint256 indexed _editionNumber,
+        uint256 _amount
+      );
+    */
     let contract = KnownOrigin.bind(Address.fromString("0xFBeef911Dc5821886e1dda71586d90eD28174B7d"))
     let editionEntity = loadOrCreateEdition(event.params._editionNumber, contract)
 }
 
-
+export function handleAuctionCancelled(event: AuctionCancelled): void {
+    /*
+      event AuctionCancelled(
+        uint256 indexed _editionNumber
+      );
+    */
+    let contract = KnownOrigin.bind(Address.fromString("0xFBeef911Dc5821886e1dda71586d90eD28174B7d"))
+    let editionEntity = loadOrCreateEdition(event.params._editionNumber, contract)
+}
