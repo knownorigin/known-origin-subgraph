13a5bfada6940fb63800514174a4887f9cb04b6f
Andy Gray <andy@whatsthescore.co.uk>
2019-12-18 12:11:53 +0000

    updates

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import {BigDecimal, BigInt, EthereumEvent} from "@graphprotocol/graph-ts/index";
-import {DAY_ZERO, ONE_ETH, SECONDS_IN_DAY} from "./constants";
+import {ONE_ETH, SECONDS_IN_DAY} from "./constants";
 
 export function toEther(value: BigInt): BigDecimal {
     return new BigDecimal(value) / ONE_ETH
@@ -8,7 +8,6 @@ export function toEther(value: BigInt): BigDecimal {
 export function dayNumberFromEvent(event: EthereumEvent): string {
     return event.block.timestamp
         .div(SECONDS_IN_DAY)
-        .minus(DAY_ZERO)
         .toBigDecimal()
         .truncate(0)
         .toString()
