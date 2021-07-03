280a913e6a143872bc4222f0c8958dcf20c3a3cd
James Morgan <james.morgan.e@googlemail.com>
2021-07-03 07:15:16 +0100

    More collab mapping

--- src/mappings/v3/known-origin-v3-collab-registry-mapping.ts
+++ src/mappings/v3/known-origin-v3-collab-registry-mapping.ts
@@ -5,35 +5,63 @@ import {
     RoyaltiesHandlerSetup,
     RoyaltyRecipientCreated
 } from "../../../generated/KODAV3CollabRegistry/KODAV3CollabRegistry";
-import {CollabHandlers} from "../../../generated/schema";
-import { Bytes} from "@graphprotocol/graph-ts/index";
+import {BaseCollabHandlers, CollabHandlers} from "../../../generated/schema";
+import {Bytes} from "@graphprotocol/graph-ts/index";
 
 export function handleRoyaltyRecipientCreated(event: RoyaltyRecipientCreated): void {
-    let collab = CollabHandlers.load(event.params.deployedHandler.toHexString())
-    if (collab == null) {
-        collab = new CollabHandlers(event.params.deployedHandler.toHexString());
-    }
+    let collab = loadCollabHandler(event.params.deployedHandler.toHexString())
     collab.baseHandler = event.params.handler;
     collab.creator = event.params.creator;
     collab.recipients = event.params.recipients.map<Bytes>(a => (Bytes.fromHexString(a.toHexString()) as Bytes))
     collab.splits = event.params.splits;
     collab.createdTimestamp = event.block.timestamp
     collab.transactionHash = event.transaction.hash
+    collab.isDeployed = true
     collab.save()
 }
 
 export function handleRoyaltiesHandlerSetup(event: RoyaltiesHandlerSetup): void {
-
+    let collab = loadCollabHandler(event.params.deployedHandler.toHexString())
+    collab.editionNmber = event.params.editionId
+    collab.isDeployed = true
+    collab.save()
 }
 
 export function handleFutureRoyaltiesHandlerSetup(event: FutureRoyaltiesHandlerSetup): void {
-
+    let collab = loadCollabHandler(event.params.deployedHandler.toHexString())
+    collab.editionNmber = event.params.editionId
+    collab.isDeployed = false
+    collab.save()
 }
 
 export function handleHandlerAdded(event: HandlerAdded): void {
-
+    let baseHandler = loadBaseCollabHandler(event.params.handler.toHexString());
+    baseHandler.active = true
+    baseHandler.lastUpdatedTimestamp = event.block.timestamp
+    baseHandler.lastUpdatedTransactionHash = event.transaction.hash
+    baseHandler.save()
 }
 
 export function handleHandlerRemoved(event: HandlerRemoved): void {
+    let baseHandler = loadBaseCollabHandler(event.params.handler.toHexString());
+    baseHandler.active = false
+    baseHandler.lastUpdatedTimestamp = event.block.timestamp
+    baseHandler.lastUpdatedTransactionHash = event.transaction.hash
+    baseHandler.save()
+}
 
+function loadCollabHandler(id: string): CollabHandlers {
+    let collab = CollabHandlers.load(id)
+    if (collab == null) {
+        collab = new CollabHandlers(id);
+    }
+    return collab as CollabHandlers;
+}
+
+function loadBaseCollabHandler(id: string): BaseCollabHandlers {
+    let handler = BaseCollabHandlers.load(id)
+    if (handler == null) {
+        handler = new BaseCollabHandlers(id);
+    }
+    return handler as BaseCollabHandlers;
 }
