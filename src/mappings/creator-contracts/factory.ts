500eeef6978ce248c1fa32615b298f9bf5f79d83
Vincent <vince0656@googlemail.com>
2022-08-04 11:01:47 +0100

    CC splits, percentages, ERC165 and more being indexed

--- src/mappings/creator-contracts/factory.ts
+++ src/mappings/creator-contracts/factory.ts
@@ -1,6 +1,15 @@
 import {
-    SelfSovereignERC721Deployed
+    SelfSovereignERC721Deployed,
+    CreatorContractBanned
 } from "../../../generated/KnownOriginV4Factory/KnownOriginV4Factory";
+
+import {
+    FundsHandler
+} from "../../../generated/KnownOriginV4Factory/FundsHandler";
+
+import {
+    BatchCreatorContract
+} from "../../../generated/KnownOriginV4Factory/BatchCreatorContract";
 
 import {
     CreatorContract
@@ -10,20 +19,63 @@ import {
     CreatorContract as CreatorContractTemplate
 } from '../../../generated/templates';
 
+import {Bytes, BigInt} from "@graphprotocol/graph-ts/index";
+import {ZERO, ONE} from "../../utils/constants";
+
 export function handleSelfSovereignERC721Deployed(event: SelfSovereignERC721Deployed): void {
     // Capture the contract global properties so the list of creator contracts can be fetched
-    let contract = new CreatorContract(event.params.selfSovereignNFT.toHexString())
-    contract.deploymentBlockNumber = event.block.number
-    contract.deploymentTimestamp = event.block.timestamp
-    contract.implementation = event.params.implementation
-    contract.deployer = event.params.deployer
-    contract.creator = event.params.artist
-    contract.defaultFundsHandler = event.params.fundsHandler
-    contract.owner = event.params.artist
-    contract.minter = event.params.artist
-    contract.isHidden = false
-    contract.save()
+    let creatorContractEntity = new CreatorContract(event.params.selfSovereignNFT.toHexString())
+    creatorContractEntity.deploymentBlockNumber = event.block.number
+    creatorContractEntity.deploymentTimestamp = event.block.timestamp
+    creatorContractEntity.implementation = event.params.implementation
+    creatorContractEntity.deployer = event.params.deployer
+    creatorContractEntity.creator = event.params.artist
+    creatorContractEntity.owner = event.params.artist
+    creatorContractEntity.minter = event.params.artist
+    creatorContractEntity.isHidden = false
 
-    // Inform the subgraph to index events from the creator contract
+    // ERC165 interface lookup
+    let sovereignContractInstance = BatchCreatorContract.bind(event.params.selfSovereignNFT)
+    creatorContractEntity.isBatchBuyItNow = sovereignContractInstance.supportsInterface(Bytes.fromHexString("0x0c7cb431") as Bytes)
+    if (creatorContractEntity.isBatchBuyItNow) {
+        creatorContractEntity.ERC165InterfaceID = Bytes.fromHexString("0x0c7cb431") as Bytes
+    }
+
+    // Inform the subgraph to index events from the creator creatorContractEntity
     CreatorContractTemplate.create(event.params.selfSovereignNFT)
+
+    // Check if the funds handler is a self sovereign funds handler or just a regular address
+    creatorContractEntity.defaultFundsHandler = event.params.fundsHandler
+    let maybeFundsHandlerContract = FundsHandler.bind(event.params.fundsHandler)
+    let maybeTotalRecipientsResult = maybeFundsHandlerContract.try_totalRecipients()
+
+    let defaultFundsRecipients = new Array<Bytes>()
+    let defaultFundsShares = new Array<BigInt>()
+    if (maybeTotalRecipientsResult.reverted == false) {
+        let totalRecipients = maybeTotalRecipientsResult.value
+
+        for(let i = ZERO; i.lt(totalRecipients); i = i.plus(ONE)) {
+            let share = maybeFundsHandlerContract.shareAtIndex(i)
+            defaultFundsRecipients.push(share.value0)
+            defaultFundsShares.push(share.value1)
+        }
+
+        creatorContractEntity.defaultFundsRecipients = defaultFundsRecipients
+        creatorContractEntity.defaultFundsShares = defaultFundsShares
+    } else {
+        // in this case there is just 1 default address that will receive 100%
+        defaultFundsRecipients.push(event.params.fundsHandler)
+        defaultFundsShares.push(BigInt.fromString("10000000"))
+
+        creatorContractEntity.defaultFundsRecipients = defaultFundsRecipients
+        creatorContractEntity.defaultFundsShares = defaultFundsShares
+    }
+
+    creatorContractEntity.save()
+}
+
+export function handleCreatorContractBanned(event: CreatorContractBanned): void {
+    let creatorContractEntity = CreatorContract.load(event.params.contractAddress.toHexString())
+    creatorContractEntity.isHidden = true
+    creatorContractEntity.save()
 }
