1b682a9db3a6bf44b59377491aba2bcf496a71a7
Vincent <vince0656@googlemail.com>
2022-08-18 11:54:01 +0100

    log whether transfer is called on creator contract

--- src/mappings/creator-contracts/BatchCreatorContract.ts
+++ src/mappings/creator-contracts/BatchCreatorContract.ts
@@ -9,7 +9,10 @@ import {
     loadOrCreateV4EditionFromTokenId
 } from "../../services/Edition.service";
 
+import {log} from "@graphprotocol/graph-ts/index";
+
 export function handleTransfer(event: Transfer): void {
+    log.info("Calling handleTransfer() call for contract {} ", [event.address.toHexString()])
     if (event.params.from.equals(ZERO_ADDRESS)) { // Mint
         let edition = loadOrCreateV4EditionFromTokenId(
             event.params.tokenId,
