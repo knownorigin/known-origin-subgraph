417818d37cc68278d41cc3013dba57ed5cf8e969
Vincent <vince0656@googlemail.com>
2022-11-24 10:45:13 +0000

    fix resetting to bids too early for v4 editions

--- src/mappings/creator-contracts/creator-contract.ts
+++ src/mappings/creator-contracts/creator-contract.ts
@@ -317,7 +317,9 @@ export function handleBuyNowPurchased(event: BuyNowPurchased): void {
     edition.totalSupply = edition.totalSupply + ONE;
     edition.remainingSupply = edition.remainingSupply - ONE
     edition.totalSold = edition.totalSold + ONE
-    edition.salesType = SaleTypes.OFFERS_ONLY // Revert back to the default state
+    if (edition.remainingSupply.equals(ZERO)) {
+        edition.salesType = SaleTypes.OFFERS_ONLY // Revert back to the default state
+    }
 
     let tokenIds = edition.tokenIds
     tokenIds.push(event.params._tokenId)
