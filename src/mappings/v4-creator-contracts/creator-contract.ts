619e18a187bb197d574b880f98cb3ead21ed4a93
Matt <matwilkinson@ebay.com>
2023-03-15 15:44:09 +0000

    change way artist address is fetched

--- src/mappings/v4-creator-contracts/creator-contract.ts
+++ src/mappings/v4-creator-contracts/creator-contract.ts
@@ -420,10 +420,10 @@ export function handleListedForBuyItNow(event: ListedEditionForBuyNow): void {
         activityEventService.recordEditionCreated(event, edition);
 
         // Add Open Edition to Artist
-        let owner = creatorContractInstance.owner()
         let isNewEdition = Edition.load(createV4Id(event.address.toHexString(), edition.id.toString())) == null
-        if (isNewEdition) {
-            addEditionToArtist(owner, edition.id, edition.totalAvailable, event.block.timestamp, KODA_V4)
+        if(isNewEdition) {
+            let artist = Address.fromString(edition.artistAccount.toHexString());
+            addEditionToArtist(artist, edition.id, edition.totalAvailable, event.block.timestamp, KODA_V4)
         }
     }
 
